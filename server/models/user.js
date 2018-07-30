892b77dc5d7d3f6aadceb15b968fea03080c84ed
venus <venus@venusworld.cn>
2018-07-30 21:41:38 +0800

    refactor: update model

--- server/models/user.js
+++ server/models/user.js
@@ -10,6 +10,10 @@ let userSchema = new Schema({
   username: String,
   password: String,
   friends: [String],
+  avatarURL: {
+    type: String,
+    default: 'https://www.gravatar.com/avatar/a5717a649d346ed0c51be68888c130cd?f=y&d=identicon'
+  },
   createdAt: {
     type: Date,
     default: Date.now
